f5a3bd69bb0195118351bcfcfa7ff727c8d4bcb7
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2021-11-10 20:29:50 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 import './style.css'
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { GUI } from 'dat.gui'
+import Stats from 'stats.js'
+import * as CANNON from 'cannon-es'
+import cannonDebugger from 'cannon-es-debugger'
 const canvas = document.querySelector('.webgl')
 
 class NewScene{
@@ -11,9 +15,13 @@ class NewScene{
     _Init(){
         this.scene = new THREE.Scene()
         this.clock = new THREE.Clock()
+        this.InitStats()
+        this.InitPhysics()
+        this.InitPhysicsDebugger()
         this.InitCamera()
         this.InitEnv()
         this.InitBuildings()
+        this.InitLights()
         this.InitRenderer()
         this.InitControls()
         this.Update()
@@ -22,37 +30,87 @@ class NewScene{
         })
     }
 
+    InitStats(){
+        this.stats = new Stats()
+        document.body.appendChild(this.stats.dom)
+    }
+
+    InitPhysics(){
+        this.world = new CANNON.World()
+        this.world.gravity.set(0, -9.82, 0)
+        this.defaultMaterial = new CANNON.Material('default')
+        this.defaultContactMaterial = new CANNON.ContactMaterial(
+            this.defaultMaterial,
+            this.defaultMaterial,
+            {
+                friction: 0.1,
+                restitution: 0.2
+            }
+        )
+        this.world.broadphase = new CANNON.SAPBroadphase(this.world)
+        this.world.allowSleep = true
+        this.world.defaultContactMaterial = this.defaultContactMaterial
+        this.world.addContactMaterial(this.defaultContactMaterial)
+    }
+
+    InitPhysicsDebugger(){
+        cannonDebugger(
+            this.scene,
+            this.world.bodies,
+            {
+                color: 0xff00ff,
+                autoUpdate: true
+            }
+        )
+    }
+
     InitEnv(){
         this.planeGeomtery = new THREE.PlaneGeometry(1000, 1000)
-        this.planeMaterial = new THREE.MeshNormalMaterial({
+        this.planeMaterial = new THREE.MeshStandardMaterial({
+            color: 0x144552,
             side: THREE.DoubleSide
         })
         this.plane = new THREE.Mesh(this.planeGeomtery, this.planeMaterial)
         this.plane.rotation.x = -Math.PI * 0.5
         this.scene.add(this.plane)
+
+        this.groundBody = new CANNON.Body({
+            mass: 0,
+            material: this.defaultMaterial
+        })
+        this.world.addBody(this.groundBody)
+        this.groundBody.addShape(new CANNON.Plane())
+        this.groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5)
     }
 
     InitBuildings(){
-        let x, y, z
-        this.rand = 10 + Math.random() * 25;
-        this.buildingGeometry = new THREE.BoxGeometry(x, this.rand, z)
         this.buildingMaterial = new THREE.MeshNormalMaterial()
-        for (let i = 0; i <= 100; i++){
-            x = Math.round(Math.random()*20+10)
-            z = Math.round(Math.random()*20+10)
-            this.angle = Math.random() * Math.PI * 2
-            this.radius = 5 + Math.random() * 100
+        for (let i = 0; i <= 200; i++){
+            this.rand = 50 + Math.random() * 50;
+            this.x = Math.round(Math.random()*20+10)
+            this.z = Math.round(Math.random()*20+10)
+            this.angle = Math.random() * Math.PI * 20
+            this.radius = 100 + Math.random() * 500
             this.posX = Math.cos(this.angle) * this.radius
             this.posZ = Math.sin(this.angle) * this.radius
-            this.building = new THREE.Mesh(this.buildingGeometry, this.buildingMaterial)
+            this.building = new THREE.Mesh(new THREE.BoxGeometry(this.x, this.rand, this.z), this.buildingMaterial)
             this.scene.add(this.building)
             this.building.position.set(this.posX, this.rand/2, this.posZ)
+        
+            this.buildingShape = new CANNON.Box(new CANNON.Vec3(this.x/2, this.rand/2, this.z/2))
+            this.buildingBody = new CANNON.Body({
+                mass: 0,
+                material: this.defaultMaterial
+            })
+            this.buildingBody.position.set(this.posX, this.rand/2, this.posZ)
+            this.buildingBody.addShape(this.buildingShape)
+            this.world.addBody(this.buildingBody)
         }
     }
 
     InitCamera(){
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-        this.camera.position.set(0, 0.5, 4)
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000)
+        this.camera.position.set(0, 500, 500)
         this.scene.add(this.camera)
     }
 
@@ -74,9 +132,11 @@ class NewScene{
     }
 
     InitControls(){
-        this.controls = new OrbitControls(this.camera, canvas)
-        this.controls.enableDamping = true
-        this.controls.update()
+        
+            this.controls = new OrbitControls(this.camera, canvas)
+            this.controls.enableDamping = true
+            this.controls.update()
+        
     }
 
     Resize(){
@@ -86,9 +146,10 @@ class NewScene{
     }
 
     Update(){
-        requestAnimationFrame(() => {     
+        requestAnimationFrame(() => {
             this.renderer.render(this.scene, this.camera)
             this.controls.update()
+            this.stats.update() 
             this.Update()
         })  
     }
